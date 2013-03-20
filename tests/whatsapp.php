d5d557ba2f50728bb87855b3e3089ee26a02c5f6
federicovsk <phenomena31@yahoo.it>
2013-03-20 12:49:00 +0100

    Update whatsapp.php

--- tests/whatsapp.php
+++ tests/whatsapp.php
@@ -15,9 +15,9 @@ function fgets_u($pStdn)
     }
 }
 
-$nickname = "WhatsAPI Test";
-$sender = 	""; // Mobile number with country code (but without + or 00)
-$imei = 	""; // MAC Address for iOS IMEI for other platform (Android/etc)
+$nickname = "Danae";
+$sender = 	"5219841385419"; // Mobile number with country code (but without + or 00)
+$imei = 	"356180048676464"; // MAC Address for iOS IMEI for other platform (Android/etc)
 
 if ($argc < 2) {
     echo "USAGE: ".$_SERVER['argv'][0]." [-l] [-s <phone> <message>] [-i <phone>] [-set <status>]\n";
